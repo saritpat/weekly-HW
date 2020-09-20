1da57837a5cd2ea5cd4e6d57699d985286c107d4
saritpat <69256964+saritpat@users.noreply.github.com>
2020-09-20 20:43:07 +0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    int N,P;
+    scanf("%d",&N);
+    P = 1;
+    while(P <= N)
+    {
+        for(int s = 1;s <= (N-P);s++)
+        {
+            printf(" ");
+        }
+        for(int t = 1;t <= 2*P-1;t++)
+        {
+            printf("*");
+        }
+        printf("\n");
+        P++;
+    }
+    P = N-1;
+    while(P >= 1)
+    {
+        for(int s = 1;s <= (N-P);s++)
+        {
+            printf(" ");
+        }
+        for(int t = 1;t <= 2*P-1;t++)
+        {
+            printf("*");
+        }
+        printf("\n");
+        P--;
+    }
+    return 0;
+}
