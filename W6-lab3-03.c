9099a264eeb6ca082c7be340abba1098efdb1fac
saritpat <69256964+saritpat@users.noreply.github.com>
2020-09-20 14:17:37 +0700

    Create W6-lab3-03.c

---
+++ W6-lab3-03.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int N,P,Q;
+    scanf("%d",&N);
+    P = 1;
+    Q = N-1;
+    while(P <= N)
+    {
+        for(int t = 0;t < P;t++)
+        {
+            printf("*");
+        }
+        for(int s = 1;s <= 2*Q-1;s++)
+        {
+            printf(" ");
+        }
+        if(P != N)
+        {
+           for(int t = 0;t < P;t++)
+            {
+                printf("*");
+            }
+        }
+        else
+        {
+            for(int t = 1;t < P;t++)
+            {
+                printf("*");
+            }
+        }
+        printf("\n");
+        P++;
+        Q--;
+    }
+    P = N+1;
+    Q = 1;
+    while(P > 2)
+    {
+        for(int t = 0;t <= P-3;t++)
+        {
+            printf("*");
+        }
+        for(int s = 1;s <= 2*Q-1;s++)
+        {
+            printf(" ");
+        }
+        for(int t = 0;t <= P-3;t++)
+        {
+            printf("*");
+        }
+        printf("\n");
+        P--;
+        Q++;
+    }
+    return 0;
+}
