fe32603eee21af83a88f083d7a32a65d32049de7
saritpat <69256964+saritpat@users.noreply.github.com>
2020-11-22 21:22:48 +0700

    Update Ww13-Up Down.cpp

--- Ww13-Up Down.cpp
+++ Ww13-Up Down.cpp
@@ -3,7 +3,6 @@
 
 
 void Up(char*);
-void Down(char*);
 
 
 void Up(char* str)
@@ -14,31 +13,16 @@ void Up(char* str)
 	}
 }
 
-void Down(char* str)
-{
-	if (*str >= 'A' && *str <= 'Z')
-	{
-		*str += 32;
-	}
-}
-
 int main()
 {
 	char str[1000];
-	int ai = 0;
 	scanf("%[^\n]", str);
-	for (int a = 0; str[a] != '\0'; a++)
+	int a = 0;
+	while (str[a] != '\0')
 	{
-		if (*str >= 'a' && *str <= 'z')
-		{
-			Up(&str[a]);
-		}
-		if (*str >= 'A' && *str <= 'Z')
-		{
-			Down(&str[a]);
-		}
+		Up(&str[a]);
 		printf("%c", str[a]);
+		a++;
 	}
-
 	return 0;
 }
